df4c985c48af0113bf4e02894a281d442742a8ce
Sergii Demianchuk <sergeydemjanchyk@gmail.com>
2024-11-11 21:30:50 -0500

    Fix spec and rubocop offenses

--- spec/requests/links_spec.rb
+++ spec/requests/links_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Links", type: :request do
-  describe "GET /all" do
+  describe "GET /index" do
     it "returns http success" do
-      get "/links/all"
+      get "/links"
       expect(response).to have_http_status(:success)
     end
   end
-
 end
