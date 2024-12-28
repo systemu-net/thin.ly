223a5317ba1ab5d6672f660df073d0ee8b50aa4e
Sergii Demianchuk <sergeydemjanchyk@gmail.com>
2024-12-27 22:02:11 -0500

    Add gem annotate

--- spec/models/qr_code_spec.rb
+++ spec/models/qr_code_spec.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: qr_codes
+#
+#  id         :integer          not null, primary key
+#  image      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  link_id    :integer          not null
+#  user_id    :integer          not null
+#
+# Indexes
+#
+#  index_qr_codes_on_link_id  (link_id)
+#  index_qr_codes_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  link_id  (link_id => links.id)
+#  user_id  (user_id => users.id)
+#
 require 'rails_helper'
 require 'carrierwave/test/matchers'
 
