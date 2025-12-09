23c3df7ac9b72631a3509c3aeade5ee97e8a1809
Sergii Demianchuk <sergeydemjanchyk@gmail.com>
2025-12-09 03:13:07 -0500

    Update redis config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,7 @@
 unless Rails.env.test?
-  $redis = Redis.new(url: ENV.fetch("REDIS_URL"))
+  $redis = Redis.new(
+    url: ENV.fetch("REDIS_URL"),
+    timeout: 10, # Increase timeout for slower networks
+    reconnect_attempts: 3
+  )
 end
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,17 @@
 unless Rails.env.test?
   Sidekiq.configure_server do |config|
-    config.redis = { url: ENV.fetch("REDIS_URL") }
+    config.redis = {
+      url: ENV.fetch("REDIS_URL"),
+      timeout: 10,
+      reconnect_attempts: 3
+    }
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = { url: ENV.fetch("REDIS_URL") }
+    config.redis = {
+      url: ENV.fetch("REDIS_URL"),
+      timeout: 10,
+      reconnect_attempts: 3
+    }
   end
 end
