6e8af10babf5c7a325394860b88ef8a366371d16
Sergii Demianchuk <sergeydemjanchyk@gmail.com>
2025-12-08 21:35:40 -0500

    Correct OPENAI_ACCESS_TOKEN

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,7 +1,6 @@
 unless Rails.env.test?
   OpenAI.configure do |config|
-    config.access_token = ENV.fetch("OPENAI_ACCESS_TOKEN") ||
-      Rails.application.credentials.fetch(:openai_access_token, nil)
+    config.access_token = ENV["OPENAI_ACCESS_TOKEN"] || Rails.application.credentials.fetch(:openai_access_token, nil)
     # Highly recommended in development, so you can see what errors OpenAI is returning.
     # Not recommended in production because it could leak private data to your logs.
     config.log_errors = true if Rails.env.development?
