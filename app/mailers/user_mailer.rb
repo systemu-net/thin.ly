bdef01bf40c2c1f6b977d8dba00b321fcd892ecb
Sergii Demianchuk <sergeydemjanchyk@gmail.com>
2025-12-09 21:30:45 -0500

    Fix UserMailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ApplicationMailer
   def created(user)
     @created_user_email = user.email
 
-    mail to: ENV["NOTIFICATION_EMAIL"], subject: "New user created"
+    mail to: ENV["NOTIFICATION_EMAIL"] || Rails.application.credentials.notification_email, subject: "New user created"
   end
 end
