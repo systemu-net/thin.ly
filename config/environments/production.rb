2564b3d44b601ef5711bba0154a91d48c47a869e
Sergii Demianchuk <sergeydemjanchyk@gmail.com>
2025-12-13 13:07:39 -0500

    Update mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,9 +79,21 @@ Rails.application.configure do
   # caching is enabled.
   config.action_mailer.perform_caching = false
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  # ActionMailer configuration for production
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    address: "smtp.mailgun.org",
+    port: 587,
+    domain: "thin.ly",
+    user_name: Rails.application.credentials.dig(:smtp_user_name),
+    password: Rails.application.credentials.dig(:smtp_password),
+    authentication: "plain",
+    enable_starttls_auto: true,
+    open_timeout: 5,
+    read_timeout: 5
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -110,6 +122,6 @@ Rails.application.configure do
   ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  # TODO: remove/refactor this
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+
+  config.action_mailer.default_url_options = { host: "thin.ly" }
 end
