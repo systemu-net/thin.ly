93522d5480d3ab5b8f4867bd457bc62a0afbb7e3
Sergii Demianchuk <sergeydemjanchyk@gmail.com>
2025-12-13 12:35:36 -0500

    Optimize puma config

--- config/puma.rb
+++ config/puma.rb
@@ -25,6 +25,9 @@ workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 # process behavior so workers use less memory.
 preload_app!
 
+# Silence single worker warning when running on resource-constrained instances
+silence_single_worker_warning
+
 # If you are preloading your application and using Active Record, it's
 # recommended that you close any connections to the database before workers
 # are forked to prevent connection leakage.
